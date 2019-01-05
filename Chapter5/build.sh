c18e39d61fb9e5cc03707e4e4d1a92891aa0ecea
wuzhenhai <you@example.com>
2019-01-04 19:11:44 -0800

    code

--- Chapter5/build.sh
+++ Chapter5/build.sh
@@ -1,3 +1,7 @@
 gcc server_5_2_str_echo.c ../Chapter3/read_write_helper.c -I.. -o x1
 
 gcc client_5_3_str_cli.c ../Chapter3/read_write_helper.c -I.. -o x2
+
+gcc server_5_12_str_echo.c ../Chapter3/read_write_helper.c -I.. -o x3
+
+gcc server_5_17_str_echo.c ../Chapter3/read_write_helper.c -I.. -o x4
