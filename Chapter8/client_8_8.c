193b46fbfc5ecd72166d6e59fab21cc95c9b7847
wuzhenhai <2663976081@qq.com>
2019-03-01 01:35:32 -0800

    code

--- Chapter8/client_8_8.c
+++ Chapter8/client_8_8.c
@@ -15,7 +15,12 @@ void dg_cli(FILE *fp, int sockfd, const struct sockaddr_in *pservaddr, socklen_t
         sendto(sockfd, sendline, strlen(sendline), 0, (const struct sockaddr *)pservaddr, servlen);
         socklen_t len = servlen;
         int n = recvfrom(sockfd, recvline, MAXLINE, 0, preply_addr, &len);
-        //比如从服务器返回的地址是不是和发送目标地址一致
+        //比较从服务器返回的地址是不是和发送目标地址一致
+        /*
+            这样比较仍然会有问题：
+                当服务器是一个多宿主机时，服务器的外出接口可能不是客户端发送数据报的目的IP地址
+                这样检测就会失败
+        */
         if(len!=servlen || !memcmp(pservaddr, preply_addr, len))
         {
             printf("reply from %s (ignored)\n", sock_ntop((const struct sockaddr*)preply_addr, len));
